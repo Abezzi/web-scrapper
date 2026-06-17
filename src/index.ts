fb8fd0d74168d60ddd86452ff4aa98ccc4b140bd
abezzi <abezzi@outlook.cl>
2026-06-17 14:42:17 -0400

    feat(main): reads one argument (base url) from command cli

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log("Hello, World!");
+import { argv } from "node:process";
+
+function main() {
+  if (!argv[2]) {
+    console.log("ERROR, NO ARGUMENTS");
+    process.exit(1);
+  }
+
+  if (argv[3]) {
+    console.log("ERROR, TOO MANY ARGUMENTS");
+    process.exit(1);
+  }
+
+  // argv.forEach((val, index) => {
+  //   console.log(`arg ${index}: ${val}`);
+  // });
+
+  console.log(`Crawler started at: ${argv[2]}`);
+}
+
+main();
